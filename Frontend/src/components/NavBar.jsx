a0b1c059472dd1e5eed41a79d09e7464882cf7c6
maaz-official <muhammadmaaz07@outlook.com>
2024-06-23 11:18:20 +0500

    updated this update page

--- Frontend/src/components/NavBar.jsx
+++ Frontend/src/components/NavBar.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
@@ -23,29 +22,3 @@ export default function NavBar() {
     </div>
   );
 }
-=======
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
-
-export default function NavBar() {
-  return (
-    <div style={{ marginTop: '3px' }}>
-
-    <AppBar position="static" style={{ backgroundColor: 'white' }}>
-      <Toolbar>
-        <Typography variant="h6" style={{ flexGrow: 1, color: 'black' }}>
-          Muhammad Maaz
-        </Typography>
-        <Button component={Link} to="/" color="inherit" style={{ color: 'black' }}>
-          Home
-        </Button>
-        <Button component={Link} to="/create-user" color="inherit" style={{ color: 'black' }}>
-          Create Profile
-        </Button>
-      </Toolbar>
-      </AppBar>
-    </div>
-  );
-}
->>>>>>> c84e2d8fdc3d275c5d15537a4d6cb24c481a1cbb
--- Frontend/src/pages/NotFound.jsx
+++ Frontend/src/pages/NotFound.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { Container, Typography } from '@mui/material';
 
@@ -16,22 +15,3 @@ const NotFound = () => {
 };
 
 export default NotFound;
-=======
-import React from 'react';
-import { Container, Typography } from '@mui/material';
-
-const NotFound = () => {
-  return (
-    <Container maxWidth="sm" style={{ textAlign: 'center', marginTop: '100px' }}>
-      <Typography variant="h2" gutterBottom>
-        Oops! Page Not Found
-      </Typography>
-      <Typography variant="body1" gutterBottom>
-        The page you are looking for does not exist.
-      </Typography>
-    </Container>
-  );
-};
-
-export default NotFound;
->>>>>>> c84e2d8fdc3d275c5d15537a4d6cb24c481a1cbb
